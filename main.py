9538705d69707228d62f7368dd063111852fe244
BaptLaMenace01 <baptquaino@hotmail.fr>
2025-07-13 13:25:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -52,4 +52,16 @@ def test():
 
 if __name__ == '__main__':
     app.run()
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/test')
+def test():
+    return '✅ Le bot est bien déployé et joignable.'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
+
+
     
